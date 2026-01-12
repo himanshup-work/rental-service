2d117ebe6ca7fd7cd3d67165fe1d930e8c1b8503
himanshu-inexture <himanshup.inexture@gmail.com>
2026-01-12 18:30:55 +0530

    fix: fix issue and ran the application

--- admin-service/src/main/java/com/admin_service/feign/AuthServiceClient.java
+++ admin-service/src/main/java/com/admin_service/feign/AuthServiceClient.java
@@ -3,21 +3,29 @@ package com.admin_service.feign;
 import com.admin_service.dtos.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "auth-service")
+@FeignClient(
+        name = "auth-service",
+        url = "http://localhost:8081"
+)
 public interface AuthServiceClient {
 
     @GetMapping("/auth/getAllUsers")
     UserDto getAllUsers(@RequestParam(required = false) String role);
 
-    @GetMapping("/auth/getUserById")
-    UserDto getUserById(Long userId);
+    @GetMapping("/auth/users/{id}")
+    UserDto getUserById(@PathVariable("id") Long userId);
 
-    @GetMapping("/auth/getUserByEmail")
-    UserDto getUserByEmail(String email);
+    @GetMapping("/auth/users/email/{email}")
+    UserDto getUserByEmail(@PathVariable("email") String email);
 
-    @PutMapping("/auth/updateUserRole")
-    void updateUserRole(Long userId, String owner);
+    @PutMapping("/auth/users/{id}/role/{role}")
+    void updateUserRole(
+            @PathVariable("id") Long userId,
+            @PathVariable("role") String owner
+    );
+
 }
